d974f2c956e8980735d63953ec8f942772bb073d
raielin@gmail.com <raielin@gmail.com>
2014-10-30 01:17:29 -0400

    Notes for initial skeleton program.

--- main.js
+++ main.js
@@ -2,19 +2,34 @@ var blackJack = {};
 
 blackJack.Ref = new Firebase('https://scorching-inferno-9253.firebaseio.com/');
 
-blackJack.Player = function(name) {
-  this.name = name;
+blackJack.Player = function(options) {
+  this.name = options.name;
   this.hand = [];
   this.wager = 0;
   this.availCash = 1000;
 
-}
+};
+
+
+blackJack.addPlayer = function() {
+  // Will need to include the code below
+    // var playerName = $('#playerNameInput').val();
+    // blackJack.Ref.push({name: name});
+    // $('#playerNameInput').val('');
+};
 
 $('#playerNameInput').keypress(function(e) {
   if (e.keyCode == 13) {
-    var playerName = $('#playerNameInput').val();
-    blackJack.Ref.push({name: name});
-    $('#playerNameInput').val('');
+    // Call blackJack.addPlayer function here
   }
 });
 
+blackJack.Player.prototype.hit = function() {};
+
+blackJack.Player.prototype.stay = function() {};
+
+
+// Come back to this - is Dealer a 'special' player?
+// Or is Dealer its own object that maybe 'inherits'
+// Player's properties and functions?
+blackJack.Dealer = new Player("Dealer");
